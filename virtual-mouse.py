5d552a1dcb4cba71d6b05e79cb54bbaab5a66e13
bharath-ak <44389541+bharath-ak@users.noreply.github.com>
2025-06-09 14:58:06 +0530

    Update virtual-mouse.py

--- virtual-mouse.py
+++ virtual-mouse.py
@@ -180,7 +180,7 @@ class VideoProcessor(VideoProcessorBase):
 rtc_config = RTCConfiguration(
     {
         "iceServers": [
-            {"urls": ["stun:stun.l.google.com:19302"]},
+            {"urls": ["stun:stun2.l.google.com:19302"]},
         ]
     }
 )
